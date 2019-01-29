d1b54429def09cbac40bbf09429c636efd5ae358
Lúcás Meier <cronokirby@gmail.com>
2019-01-29 13:27:58 +0100

    Add basic sprite drawing demo

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,37 @@
 #include "../include/raylib.h"
 
 
+// A Sprite we can draw. 
+// Only a finite number of sprites are available,
+// So the interior enum should be preferred to construct one.
+class Sprite {
+public:
+    enum SpriteIDX {
+        Boy1
+    };
+
+    Sprite(SpriteIDX sprite_i) : sprite_i(sprite_i) {}
+
+private:
+    SpriteIDX sprite_i;
+};
+
+
 int main() {
     InitWindow(1024, 768, "Hello World");
+
+    auto image = LoadImage("../res/dunji-sheet.png");
+    ImageResizeNN(&image, image.width * 2, image.height * 2);
+    auto sheet = LoadTextureFromImage(image);
+    UnloadImage(image);
+    SetTargetFPS(60);
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(GetColor(0xFF0000FF));
+        ClearBackground(BLACK);
+        DrawTexture(sheet, 0, 0, WHITE);
         EndDrawing();
     }
+    UnloadTexture(sheet);
     CloseWindow();
     return 0;
 }
