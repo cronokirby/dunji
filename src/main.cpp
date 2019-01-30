ca10184bbc31ee201e58cc6b6f9229bcd46f36b3
Lúcás Meier <cronokirby@gmail.com>
2019-01-30 12:23:58 +0100

    Add basic player movement

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,43 @@
 #include "../include/graphics.hpp"
 
 
+class Player {
+    Vector2 pos;
+    Sprite sprite;
+public:
+    Player() : sprite(Sprite::Boy1), pos(Vector2 { 0, 0 }) {}
+    
+    void update(float dT) {
+        if (IsKeyDown(KEY_A)) {
+            pos.x -= 200 * dT;
+        } else if (IsKeyDown(KEY_W)) {
+            pos.y -= 200 * dT;
+        } else if (IsKeyDown(KEY_D)) {
+            pos.x += 200 * dT;
+        } else if (IsKeyDown(KEY_S)) {
+            pos.y += 200 * dT;
+        }
+    }
+
+    void draw(const SpriteSheet& sheet) const {
+        sprite.draw(sheet, pos);
+    }
+};
+
+
 int main() {
     InitWindow(1024, 768, "Hello World");
 
     SpriteSheet sheet(2, "../res/dunji-sheet.png");
-    Sprite player(Sprite::Boy1);
+    Player player {};
 
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
-        player.draw(sheet, GetMousePosition());
+        float dT = GetFrameTime();
+        player.update(dT);
+        player.draw(sheet);
         EndDrawing();
     }
     CloseWindow();
