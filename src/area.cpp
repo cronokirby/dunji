3be740c2fdd3c375acdcc6031b210ea72997eb79
Lúcás Meier <cronokirby@gmail.com>
2019-01-31 20:36:33 +0100

    Add collision-less walls

--- src/area.cpp
+++ src/area.cpp
@@ -44,20 +44,46 @@ void draw_floor(const SpriteSheet& sheet, Floor tile, int x, int y) {
 }
 
 
+class Wall {
+    Vector2 pos;
+public:
+    Wall(int x, int y) {
+        // 3x 16x16 sprites
+        pos = Vector2 { x * 48, y * 48 };
+    }
+
+    Wall() : Wall(0, 0) {}
+
+    Rectangle hitbox() const {
+        return Rectangle { pos.x, pos.y, 48, 48 };
+    }
+
+    void draw(const SpriteSheet& sheet) const {
+        auto source = Rectangle { 1, 1, 1, 1 };
+        sheet.draw(source, pos);
+    }
+};
+
 class AreaImpl {
     int floor_width;
     int floor_height;
     Floor* floor_tiles;
+    int wall_count;
+    Wall* walls;
 public:
     AreaImpl(int w, int h) : floor_width(w), floor_height(h) {
         floor_tiles = new Floor[w * h];
         for (int i = 0; i < w * h; ++i) {
             floor_tiles[i] = Floor::Tile0;
         }
+        wall_count = 1;
+        walls = new Wall[1];
+        walls[0] = Wall(10, 10);
     }
 
     ~AreaImpl() {
         delete[] floor_tiles;
+        delete[] walls;
     }
 
     void draw(const SpriteSheet& sheet) const {
@@ -66,6 +92,9 @@ public:
                 auto tile = floor_tiles[y * floor_width + x];
                 draw_floor(sheet, tile, x, y);
             }
+        }
+        for (int i = 0; i < wall_count; ++i) {
+            walls[i].draw(sheet);
         }
     }
 };
