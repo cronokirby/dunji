76b3436874c0e2d3e695e0ba96add6f2acebe9de
Lúcás Meier <cronokirby@gmail.com>
2019-02-07 00:39:25 +0100

    Add basic attack animation

--- include/player.hpp
+++ include/player.hpp
@@ -10,16 +10,19 @@ class Weapon {
     Sprite sprite;
     Vector2 offset;
     float rotation;
+    bool done_slashing;
 public:
     Weapon();
     void set_orientation(Sprite::Orientation);
     void update(float);
+    bool done() const;
     void draw(const SpriteSheet&, Vector2) const;
 };
 
 class Player {
     Vector2 pos;
     Weapon weapon;
+    bool attacking;
     Sprite sprite;
     int walk_timer;
 public:
--- src/player.cpp
+++ src/player.cpp
@@ -7,37 +7,45 @@
 
 
 Weapon::Weapon() : sprite(Sprite::Knife) {
-        offset = Vector2 { 48, 78 };
-        rotation = 0.0;
-    }
+    offset = Vector2 { 48, 78 };
+    rotation = 0.0;
+    done_slashing = false;
+}
 
-    void Weapon::set_orientation(Sprite::Orientation o) {
-        sprite.orientation = o;
-        if (o == Sprite::Orientation::Left) {
-            offset.x = 0;
-        } else {
-            offset.x = 48;
-        }
+void Weapon::set_orientation(Sprite::Orientation o) {
+    sprite.orientation = o;
+    if (o == Sprite::Orientation::Left) {
+        offset.x = 0;
+    } else {
+        offset.x = 48;
     }
+}
 
-    void Weapon::update(float dT) {
-        rotation += 90 * dT;
-        if (rotation >= 90.0) {
-            rotation = 0;
-        }
+void Weapon::update(float dT) {
+    done_slashing = false;
+    rotation += 90 * 8 * dT;
+    if (rotation >= 90.0) {
+        done_slashing = true;
+        rotation = 0;
     }
+}
 
-    void Weapon::draw(const SpriteSheet& sheet, Vector2 pos) const {
-        pos.x += offset.x;
-        pos.y += offset.y;
-        auto rot = sprite.orientation == Sprite::Orientation::Left ? -rotation : rotation;
-        sprite.draw_rotated(sheet, pos, Vector2 { 24, 36 }, rot);
-    }
+bool Weapon::done() const {
+    return done_slashing;
+}
+
+void Weapon::draw(const SpriteSheet& sheet, Vector2 pos) const {
+    pos.x += offset.x;
+    pos.y += offset.y;
+    auto rot = sprite.orientation == Sprite::Orientation::Left ? -rotation : rotation;
+    sprite.draw_rotated(sheet, pos, Vector2 { 24, 36 }, rot);
+}
 
 
 Player::Player() : 
     sprite(Sprite::Boy1), 
     weapon(Weapon()),
+    attacking(false),
     pos(Vector2 { 100, 150 }),
     walk_timer(0) {}
 
@@ -59,47 +67,58 @@ Rectangle player_walk_box(Vector2 pos) {
     return Rectangle { pos.x + 9, pos.y + 48 + 21, 48 - 18, 48 - 21 };
 }
 
-
 void Player::update(const Area& area, float dT) {
-    Vector2 direction { 0, 0 };
-    if (IsKeyDown(KEY_A)) {
-        direction.x = -1;
-        sprite.orientation = Sprite::Left;
-    } else if (IsKeyDown(KEY_D)) {
-        direction.x = 1;
-        sprite.orientation = Sprite::Right;
-    }
-    weapon.set_orientation(sprite.orientation);
-
-    if (IsKeyDown(KEY_W)) {
-        direction.y = -1;
-    } else if (IsKeyDown(KEY_S)) {
-        direction.y = 1;
-    }
-    float length = sqrt(direction.x * direction.x + direction.y * direction.y);
-    float stretch = 400 * dT / length;
-    if (IsKeyDown(KEY_LEFT_SHIFT)) {
-        stretch /= 80;
-    }
-    // This is mainly to avoid the case where the length is 0, and we blow up
-    if (length >= 1) {
-        Vector2 try_move { direction.x * stretch, direction.y * stretch };
-        auto collision = player_walk_box(pos);
-        auto move = area.allowed_move(try_move, collision);
-        pos.x += move.x;
-        pos.y += move.y;
-
-        if (walk_timer >= 5) {
-            sprite.sprite_i = next_player_walk(sprite.sprite_i);
-            walk_timer = 0;
+    if (attacking) {
+        weapon.update(dT);
+        if (weapon.done()) {
+            attacking = false;
         }
-        walk_timer += 1;
     } else {
-        walk_timer = 0;
-        sprite.sprite_i = Sprite::Boy1;
-    }
+        if (IsKeyPressed(KEY_K)) {
+            sprite = Sprite { Sprite::Boy0, sprite.orientation };
+            attacking = true;
+        }
 
-    weapon.update(dT);
+        Vector2 direction { 0, 0 };
+        if (IsKeyDown(KEY_A)) {
+            direction.x = -1;
+            sprite.orientation = Sprite::Left;
+        } else if (IsKeyDown(KEY_D)) {
+            direction.x = 1;
+            sprite.orientation = Sprite::Right;
+        }
+
+        if (IsKeyDown(KEY_W)) {
+            direction.y = -1;
+        } else if (IsKeyDown(KEY_S)) {
+            direction.y = 1;
+        }
+        
+        weapon.set_orientation(sprite.orientation);
+
+        float length = sqrt(direction.x * direction.x + direction.y * direction.y);
+        float stretch = 400 * dT / length;
+        if (IsKeyDown(KEY_LEFT_SHIFT)) {
+            stretch /= 80;
+        }
+        // This is mainly to avoid the case where the length is 0, and we blow up
+        if (length >= 1) {
+            Vector2 try_move { direction.x * stretch, direction.y * stretch };
+            auto collision = player_walk_box(pos);
+            auto move = area.allowed_move(try_move, collision);
+            pos.x += move.x;
+            pos.y += move.y;
+
+            if (walk_timer >= 5) {
+                sprite.sprite_i = next_player_walk(sprite.sprite_i);
+                walk_timer = 0;
+            }
+            walk_timer += 1;
+        } else {
+            walk_timer = 0;
+            sprite.sprite_i = Sprite::Boy1;
+        }
+    }
 }
 
 void Player::draw(const SpriteSheet& sheet) const {
